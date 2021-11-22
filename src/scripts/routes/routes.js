00fbc9f71f3243150dfc6448f736aef095b05a82
mazzampr <moh.azam121@gmail.com>
2021-11-22 09:48:28 +0700

    indexed db

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
 import nowPlaying from "../views/pages/now-playing";
 import Upcoming from "../views/pages/upcoming";
 import Detail from "../views/pages/detail";
+import Like from "../views/pages/like";
 
 const routes = {
     '/': nowPlaying,
     '/now-playing': nowPlaying,
     '/upcoming': Upcoming,
     '/detail/:id': Detail,
-}
+    '/like': Like,
+};
 
 export default routes;
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -35,7 +35,21 @@ const createMovieItemTemplate = (movie) => `
     </div>
 `;
 
-export {createMovieDetailTemplate, createMovieItemTemplate};
+const createLikeButton = () => `
+    <button aria-label="like this movie" id="likeButton" class="like">
+        <i class="fa fa-heart-o" aria-hidden="true"></i>
+    </button>
+`;
+
+const createLikedButton = () => `
+    <button aria-label="like this movie" id="likeButton" class="like">
+        <i class="fa fa-heart" aria-hidden="true"></i>
+    </button>
+`;
+
+
+
+export {createMovieDetailTemplate, createMovieItemTemplate, createLikeButton, createLikedButton};
 
 
 
